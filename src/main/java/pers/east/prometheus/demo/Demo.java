af55cfcefd87216ad03870dad9247d2fcbff2d0f
eastFu <492510931@qq.com>
2022-02-07 00:08:51 +0800

    code

--- src/main/java/pers/east/prometheus/demo/Demo.java
+++ src/main/java/pers/east/prometheus/demo/Demo.java
@@ -1,5 +1,11 @@
 package pers.east.prometheus.demo;
 
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.exporter.PushGateway;
+
+import java.util.Random;
+
 public class Demo {
 
     public void readData(){
@@ -7,6 +13,51 @@ public class Demo {
     }
 
     public void writeData(){
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            //从spring上下文中取出这些实例
+            Counter counter = Counter.build()
+                    .name("blog_visit")
+                    .labelNames("blog_id")
+                    .help("counter_blog_visit")
+                    .register();
+            //注：通常只能注册1次，1个实例中重复注册会报错
+
+            Gauge gauge = Gauge.build()
+                    .name("blog_fans")
+                    .help("gauge_blog_fans")
+                    .register();
+
+            PushGateway gateway = new PushGateway("49.233.209.196:9091");
+            Random rnd = new Random();
+
+            //粉丝数先预设50
+            gauge.inc(50);
+            while (true) {
+                //随机生成1个blogId
+                int blogId = rnd.nextInt(100000);
+                //该blogId的访问量+1
+                counter.labels(blogId + "").inc();
+                //模拟粉丝数的变化
+                if (blogId % 2 == 0) {
+                    gauge.inc();
+                } else {
+                    gauge.dec();
+                }
+                //利用网关采集数据
+                gateway.push(counter, "job-counter-test");
+                gateway.push(gauge, "job-gauge-test");
+
+                //辅助输出日志
+                System.out.println("blogId:" + blogId);
+                Thread.sleep(5000);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
